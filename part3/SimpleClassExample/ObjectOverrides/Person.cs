fce63ef182763924725f98ad0caa34d8d8723695
Hari Mahat <hmahat@gmail.com>
2021-03-30 08:59:27 +0300

    Object overriding examples

---
+++ part3/SimpleClassExample/ObjectOverrides/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ObjectOverrides
+{
+    public class Person
+    {
+        public Person()
+        {
+        }
+    }
+}
---
+++ part3/SimpleClassExample/ObjectOverrides/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ObjectOverrides
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Objrect Over rides!");
+            Person p1 = new Person();
+
+
+            Console.WriteLine($"ToString {p1.ToString()}");
+            Console.WriteLine($"Hash Code {p1.GetHashCode()}");
+            Console.WriteLine($"Type: {p1.GetType()}");
+
+            Person p2 = p1;
+            object o = p2;
+            // Are the references pointing to the same Object in Memory?
+
+           if(o.Equals(p1)&& p2.Equals(o))
+            {
+                Console.WriteLine("Same Instances");
+            }
+
+        }
+    }
+}
